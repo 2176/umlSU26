714829572383afc6d620530ec538d8d7765240a4
Moses <Moses_Basaza@student.uml.edu>
2026-08-06 20:26:12 -0400

    Add calculator unit tests to GitHub Actions

---
+++ week10/calculator/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.leszko.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void addsPositiveNumbers() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void addsZero() {
+        assertEquals(5, calculator.sum(5, 0));
+    }
+
+    @Test
+    public void addsNegativeNumbers() {
+        assertEquals(-5, calculator.sum(-2, -3));
+    }
+}
